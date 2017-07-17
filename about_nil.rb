e76c856acd3a14bc3b9cf21865cd92593ac7d52e
Raleigh Littles <raleighlittles@umail.ucsb.edu>
2017-07-16 19:09:09 -0700

    Added 'about_nil.rb'

---
+++ about_nil.rb
@@ -0,0 +1,45 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+class AboutNil < Neo::Koan
+  def test_nil_is_an_object
+    ## assert_equal __, nil.is_a?(Object), "Unlike NULL in other languages"
+    assert_equal true, nil.is_a?(Object)
+  end
+
+  def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
+    # What happens when you call a method that doesn't exist.  The
+    # following begin/rescue/end code block captures the exception and
+    # makes some assertions about it.
+    begin
+      nil.some_method_nil_doesnt_know_about
+    rescue Exception => ex
+      # What exception has been caught?
+      assert_equal NoMethodError, ex.class
+
+      # What message was attached to the exception?
+      # (HINT: replace __ with part of the error message.)
+      assert_match(/undefined method/, ex.message)
+    end
+  end
+
+  def test_nil_has_a_few_methods_defined_on_it
+
+    ## assert_equal __, nil.nil?
+    assert_equal true, nil.nil?
+
+    ## assert_equal __, nil.to_s
+    assert_equal "", nil.to_s # Casting nil to a string always return the empty string
+
+    ## assert_equal __, nil.inspect
+    assert_equal "nil", nil.inspect # .inspect method returns the *representation* of the object as a string
+
+    # THINK ABOUT IT:
+    #
+    # Is it better to use
+    #    obj.nil?
+    # or
+    #    obj == nil
+    # Why?
+  end
+
+end
